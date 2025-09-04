cbe93709efe16ffc073aa43eda902b3f66ca3ec4
Mr-chintu <127465531+Mr-chintu@users.noreply.github.com>
2025-09-04 09:51:24 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,6 @@
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ") 
-# print(f"Hello, {first_name} {last_name}! Welcome to the program.")
-print("Hello ", first_name, last_name, "! Welcome to the program.")
+full_name = first_name + last_name
+# print(f"Hello, {full_name} ! Welcome to the program.")
+
+print("Hello ", full_name, "! Welcome to the program.")
